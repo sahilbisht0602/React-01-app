68ebcbd963f4586e3bed1223fc468c28070c145f
sahil bisht <sunnybisht117@gmail.com>
2023-03-25 23:15:16 +0530

    talwind added

--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -7,7 +7,7 @@ const ResturantCard = ({
   cloudinaryImageId,
 }) => {
   return (
-    <div className="card w-[300] bg-yellow-100 mx-5 my-2 shadow-lg border-radius rounded-lg">
+    <div className="card w-[300] bg-yellow-100 mx-5 my-2 border-radius rounded-lg hover:shadow-lg hover:bg-yellow-300">
       <img src={IMG_CDN + cloudinaryImageId} />
       <h2>{name}</h2>
       <h4>{cuisines}</h4>
